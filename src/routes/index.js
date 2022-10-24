d65c54a4659d8ab88e79f640c7bdcf9ecc7e7448
enzoove <83042412+enzoove@users.noreply.github.com>
2022-10-24 12:45:43 -0300

    Update index.js

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const ProductosRouter = require('./productos')
+const rutaPrincipal = Router();
+
+rutaPrincipal.use( '/productos', ProductosRouter );
+
+module.exports = rutaPrincipal;
